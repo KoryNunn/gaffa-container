fc73ec424628199f5e9a20530a71ea2898e70656
Kory Nunn <knunn187@gmail.com>
2014-03-23 01:01:37 +1000

    Initial Commit

---
+++ container.js
@@ -0,0 +1,13 @@
+var Gaffa = require('gaffa');
+
+function Container(){}
+Container = Gaffa.createSpec(Container, Gaffa.ContainerView);
+Container.prototype.type = 'container';
+
+Container.prototype.render = function(){
+    this.views.content.element =
+    this.renderedElement =
+    document.createElement(this.tagName || 'div');
+};
+
+module.exports = Container;
